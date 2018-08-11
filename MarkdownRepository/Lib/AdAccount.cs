ee23df3fe6dc2ee097cd5bc291ab976f9f175cd6
Bruce Liao <elgogle@gmail.com>
2018-08-11 09:44:21 +0800

    Ad 账号id显示为名称

---
+++ MarkdownRepository/Lib/AdAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarkdownRepository.Lib
+{
+    using System.DirectoryServices.AccountManagement;
+
+    public class AdAccount
+    {
+        public static string GetUserNameById(string userId)
+        {
+            string fullName = userId;
+            using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+            {
+                using (UserPrincipal user = UserPrincipal.FindByIdentity(context, userId))
+                {
+                    if (user != null)
+                    {
+                        fullName = user.DisplayName;
+                    }
+                }
+            }
+
+            return fullName;
+        }
+    }
+}
