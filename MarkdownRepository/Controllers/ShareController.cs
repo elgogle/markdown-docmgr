a57677a166a9d2ff00ea50525e0e0be9a9cc1125
Bruce Liao <elgogle@gmail.com>
2022-03-30 15:59:00 +0800

    修改样式、增加分享文章（未完成）

---
+++ MarkdownRepository/Controllers/ShareController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkdownRepository.Controllers
+{
+    public class ShareController : Controller
+    {
+        //
+        // GET: /Share/
+
+        // 查看全部分享的网址
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 分享功能，记录网址、Token
+        public ActionResult Add(string url, TimeSpan expireTime)
+        {
+            return Content("ok");
+        }
+
+        // 检验来自分享的网址，并跳转
+        public ActionResult Goto(string url, string token)
+        {
+            return Content("ok");
+        }
+
+        // 删除分享的网址
+        public ActionResult Delete(string url)
+        {
+            return Content("ok");
+        }
+
+        // 修改分享的过期时间
+        public ActionResult Edit(string url, TimeSpan expireTime)
+        {
+            return Content("ok");
+        }
+    }
+}
